b4f29be2465ad9cd5dd34428390be4f112617576
lizakhud <hudainatova@uchi.ru>
2025-02-10 00:58:43 +0300

    add bilders and index file

---
+++ page_object/pages/index.js
@@ -0,0 +1,7 @@
+export * from './mainPage';
+export * from './homePage';
+export * from './loginPage';
+export * from './articlePage';
+export * from './registerPage';
+export * from './settingsPage';
+export * from './editorArticlePage';
